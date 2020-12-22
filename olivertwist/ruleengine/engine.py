a6fffb75298822b26b5bc8dfac3582a75ae3b0bd
John Harrison <john.harrison@autotrader.co.uk>
2020-12-22 09:27:44 +0000

    Autowire all rules from `olivertwist.rules`

--- olivertwist/ruleengine/engine.py
+++ olivertwist/ruleengine/engine.py
@@ -5,33 +5,36 @@ Copyright (C) 2020, Auto Trader UK
 Created 15. Dec 2020 14:45
 
 """
+from importlib import import_module
 from typing import List
 
 from olivertwist.manifest import Manifest
-from olivertwist.ruleengine.rule import Rule
 from olivertwist.ruleengine.result import Result
-from olivertwist.rules.no_disabled_models import no_disabled_models
-from olivertwist.rules.no_orphaned_models import no_orphaned_models
-from olivertwist.rules.staging_has_single_source import (
-    staging_models_have_single_source,
-)
-from olivertwist.rules.no_rejoin_models import no_rejoin_models
+from olivertwist.ruleengine.rule import Rule
 
 
 class RuleEngine:
     def __init__(self, rules: List[Rule]):
         self.rules = rules
 
+    def __iter__(self):
+        return iter(self.rules)
+
     @classmethod
     def with_default_rules(cls) -> "RuleEngine":
-        return cls(
-            [
-                no_disabled_models,
-                no_orphaned_models,
-                staging_models_have_single_source,
-                no_rejoin_models,
-            ]
-        )
+        rules = cls.__autodiscover_rules_in_package()
+        return cls(rules)
 
     def run(self, manifest: Manifest) -> List[Result]:
         return [Result(rule, *rule.apply(manifest)) for rule in self.rules]
+
+    @staticmethod
+    def __autodiscover_rules_in_package():
+        rules_package = import_module("olivertwist.rules")
+        rules = []
+        for module_name, rule_module in rules_package.__dict__.items():
+            if not module_name.startswith("__"):
+                for obj in rule_module.__dict__.values():
+                    if isinstance(obj, Rule):
+                        rules.append(obj)
+        return rules
--- tests/ruleengine/test_engine.py
+++ tests/ruleengine/test_engine.py
@@ -19,3 +19,14 @@ def test_rule_engine_returns_results_for_rule_set(empty_raw_manifest):
 
     assert len(results) == 1
     assert isinstance(results[0], Result)
+
+
+def test_rule_engine_factory_method():
+    engine = RuleEngine.with_default_rules()
+
+    count = 0
+    for count, rule in enumerate(engine):
+        assert isinstance(rule, Rule)
+        count += 1
+
+    assert count >= 4, "There should be at least 4 rules by default"
